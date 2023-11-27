a71d72e2206086a591fcce22d405f98a868755be
Ronaldo Lima <ronaldo.lima@racsys.com.br>
2023-11-27 13:55:25 -0300

    FEAT: LazyLoad

--- lazyLoading_JS/js/scripts.js
+++ lazyLoading_JS/js/scripts.js
@@ -1,11 +1,20 @@
 const images = document.querySelectorAll(".img-container img");
 
 const observer = new IntersectionObserver((entries, observer) => {
+    console.log('teste');
     entries.forEach(entry => {
+
         if(!entry.IsIntersecting) return
+
+        const image = entry.target;
+
+        image.src = image.src.replace("&w=10", "&w=1000");
+
+
+
     });
-});
+}, {});
 
 images.forEach((image) => {
-    image.src = image.src.replace("&w=10", "&w=1000");
+    observer.observe(image);
 });
